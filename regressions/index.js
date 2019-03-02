f32e6820d7b1b3ca9926325f9e87ab6c546c3256
S.J. Du <sjdu@Cornerde-MacBook-Pro.local>
2019-03-02 15:25:06 +0800

    Implement gradient descent using tensorflowjs
    - Add test function using R square number to determine the accuracy
    
    - implement multivariate gradient descent
    
    - add processFeatures helper function
    
    - add standardize function

--- regressions/index.js
+++ regressions/index.js
@@ -6,15 +6,18 @@ const LinearRegression = require('./linear-regression')
 let { features, labels, testFeatures, testLabels } =  loadCSV('./cars.csv', {
     shuffle: true,
     splitTest: 50,
-    dataColumns: ['horsepower'],
+    dataColumns: ['horsepower', 'weight', 'displacement'],
     labelColumns: ['mpg']
 })
 
 const regression = new LinearRegression(features, labels, {
-    learningRate: 0.001,
-    iterations: 1
+    learningRate: 0.1,
+    iterations: 100
 })
 
-regression.train()
 
-console.log('Update M is:', regression.m, 'Update B is:', regression.b)
+regression.train()
+const r2 = regression.test(testFeatures, testLabels)
+console.log('R2 is', r2)
+
+// console.log('Update M is:', regression.weights.get(1, 0), 'Update B is:', regression.weights.get(0, 0))
--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -3,33 +3,53 @@ const _ = require('lodash')
 
 class LinearRegression {
     constructor(features, labels, options) {
-        this.features = features
-        this.labels = labels
+        this.features = this.processFeatures(features)
+        this.labels = tf.tensor(labels)
+
+
 
         this.options = Object.assign({
             learningRate: 0.1,
             iterations: 1000,
         }, options) 
 
-        this.m = 0
-        this.b = 0
+        // features now already has an addon column 11111 so we don't add 1
+        // [b, m1, m2, m3, ..., m_n]
+        this.weights = tf.zeros([this.features.shape[1],1])
+
+        // this.m = 0
+        // this.b = 0
     }
 
     gradientDescent() {
-        const currentGuessForMPG = this.features.map(row => {
-            return this.m * row[0] + this.b 
-        })
+        const currentGuesses = this.features.matMul(this.weights)
+        const differences = currentGuesses.sub(this.labels)
 
-        const bSlope = _.sum(currentGuessForMPG.map((guess, i) => {
-            return guess - this.labels[i][0]
-        })) * 2 / this.features.length
+        const slopes = this.features
+            .transpose()
+            .matMul(differences)
+            .div(this.features.shape[0])
 
-        const mSlope = _.sum(currentGuessForMPG.map((guess, i) => {
-            return -1 * this.features[i][0] * (this.labels[i][0] - guess)
-        })) * 2 / this.features.length
+        this.weights = this.weights.sub(
+            slopes.mul(this.options.learningRate)
+        )
+    }
 
-        this.m = this.m - this.options.learningRate * mSlope
-        this.b  = this.b - this.options.learningRate * bSlope
+    gradientDescent_() {
+        // const currentGuessForMPG = this.features.map(row => {
+        //     return this.m * row[0] + this.b 
+        // })
+
+        // const bSlope = _.sum(currentGuessForMPG.map((guess, i) => {
+        //     return guess - this.labels[i][0]
+        // })) * 2 / this.features.length
+
+        // const mSlope = _.sum(currentGuessForMPG.map((guess, i) => {
+        //     return -1 * this.features[i][0] * (this.labels[i][0] - guess)
+        // })) * 2 / this.features.length
+
+        // this.m = this.m - this.options.learningRate * mSlope
+        // this.b  = this.b - this.options.learningRate * bSlope
 
     }
 
@@ -37,6 +57,55 @@ class LinearRegression {
         for( let i = 0; i < this.options.iterations; i++) {
             this.gradientDescent()
         }
+    }
+
+    test(testFeatures, testLabels) {
+        testFeatures = this.processFeatures(testFeatures)
+        testLabels = tf.tensor(testLabels)
+
+
+        const predictions = testFeatures.matMul(this.weights)
+
+        // Sum of squares of residuals
+        const res = testLabels
+            .sub(predictions)
+            .pow(2)
+            .sum()
+            .get()
+
+        // Total sum of squares
+        const tot = testLabels.sub(testLabels.mean())
+            .pow(2)
+            .sum()
+            .get()
+
+        return 1 - res / tot
+    }
+
+    processFeatures(features) {
+        features = tf.tensor(features)
+
+        if(this.mean && this.variance){
+            features = features.sub(this.mean).div(this.variance.pow(0.5))
+        } else {
+            features = this.standardize(features)
+        }
+
+        features = tf.ones([features.shape[0],1]).concat(features, 1)
+
+
+        return features
+
+    }
+
+    standardize(features) {
+        const { mean, variance } = tf.moments(features, 0) 
+
+        this.mean = mean
+        this.variance = variance
+
+        return features.sub(mean).div(variance.pow(0.5))
+
     }
 }
 
